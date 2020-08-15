0a874d9db693143cab7c7e38915ad08470af445c
Gavin Wheeler <gavin.wheeler@kcl.ac.uk>
2020-08-15 17:21:49 +0100

    vtktounity#14 Set Transfer Function From Scripts - Fix for transfer functions on scene change
    The additional transfer functions were being destroyed on scene exit, but not being recreated on the entry to the new scenes. Fixed this, but making the not be destroyed on scene change, and then destroyed on application quit.

--- Scripts/VTKRendering/AddJetTransferFunction.cs
+++ Scripts/VTKRendering/AddJetTransferFunction.cs
@@ -6,7 +6,7 @@ using ThreeDeeHeartPlugins;
 public class AddJetTransferFunction : MonoBehaviour {
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         var transferFunctionId = VtkToUnityPlugin.AddTransferFunction();
 
@@ -37,9 +37,11 @@ public class AddJetTransferFunction : MonoBehaviour {
         VtkToUnityPlugin.SetTransferFunctionPoint(
             transferFunctionId,
             0.5, 0.5, 0.0, 0.0, 1.0);
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    void OnDestroy()
+    void OnApplicationQuit()
     {
         VtkToUnityPlugin.ResetTransferFunctions();
     }
